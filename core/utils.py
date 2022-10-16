1d3acdb21c1e9d16603d59918ffea1047d805d49
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-10-16 20:42:53 -0300

    conectando ao banco de dados

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,17 @@
 import MySQLdb
+
+def conectar():
+    """
+    Função para conectar ao servidor
+    :return: conn
+    """
+    try:
+        conn = MySQLdb.connect(
+            db='pmysql',
+            host='localhost',
+            user='vinic',
+            password='vinic'
+        )
+        return conn
+    except MySQLdb.Error as e:
+        print(f'Erro na conexão ao MySQL Server: {e}')
